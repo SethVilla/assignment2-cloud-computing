c7a2f28998556eeaaafa5bb10daa0d9dc9067269
Seth Villa <sethvilla13@gmail.com>
2024-08-22 04:54:59 -0700

    update or logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import {useEffect, useState} from "react";
 import axios from "axios"
 
 function App() {
+    const [isLoading, setIsLoading] = useState(true)
+    const [selected, setSelected] = useState(false)
     const [records, setRecords] = useState([]);
     useEffect(() => {
+        if (isLoading) {
             const config = {
                 headers: { Authorization: `Bearer patJp50nHhQ9KwHSg.4983b995ffe07d98ded7e41d06e92d9535f34eef22ee4e75de8cd415e500b79d` }
             };
             (async () => {
                 const data = await axios.get("https://api.airtable.com/v0/app5rf2XrERFzGafL/tblDDsknM81XswIL3",config);
-                console.log(data?.data?.records)
                 setRecords(() => data?.data?.records)
+                setIsLoading(false)
             })()
-    }, [records]);
+        }
+    }, [isLoading, records]);
 
     const onSubmit = async (event) => {
         event.preventDefault();
@@ -41,6 +45,11 @@ function App() {
             headers: { Authorization: `Bearer patJp50nHhQ9KwHSg.4983b995ffe07d98ded7e41d06e92d9535f34eef22ee4e75de8cd415e500b79d` }
         };
         await axios.post("https://api.airtable.com/v0/app5rf2XrERFzGafL/tblDDsknM81XswIL3", payload, config)
+        setIsLoading(true)
+    }
+
+    const onSelected = (e) => {
+        setSelected(e.target.name)
     }
   return (
       <>
@@ -52,11 +61,17 @@ function App() {
                       <div>College Level</div>
                       <section style={{display: "flex", alignItems: "center", justifyContent: "start", gap: "8px"}}>
                           <label htmlFor="BS" style={{width: "50px"}}>BS</label>
-                          <input type="radio" name="BS" value="BS"/>
+                          <input type="radio" name="BS" value="BS"
+                                 checked={"BS" === selected}
+                                 onChange={onSelected}
+                          />
                       </section>
                       <section style={{display: "flex", alignItems: "center", justifyContent: "start", gap: "8px"}}>
                           <label htmlFor="MS" name="MS" style={{width: "50px"}}>MS</label>
-                          <input name="MS" type="radio" value="MS"/>
+                          <input name="MS" type="radio" value="MS"
+                                 checked={"MS" === selected}
+                                 onChange={onSelected}
+                          />
                       </section>
                   </section>
                   <input name="classTaken" placeholder="class taken"/>
@@ -65,8 +80,7 @@ function App() {
               </section>
           </form>
           <section style={{margin: "8px 0"}}>
-              { records?.map((fields) => {
-                  console.log(fields)
+              {!isLoading && records?.map((fields) => {
                   return <div style={{display: "flex", gap: "8px"}} key={fields.id}>
                       <div>{fields?.fields?.Name}</div>
                       <div>{fields?.fields?.Email}</div>
